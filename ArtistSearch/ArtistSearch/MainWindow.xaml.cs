0b2d63b4d70ad02bada1f6319f8f5da348c955ba
Dman355 <daire.finn@mail.itsligo.ie>
2018-04-20 17:54:01 +0100

    Added basic artist search interface

--- ArtistSearch/ArtistSearch/MainWindow.xaml.cs
+++ ArtistSearch/ArtistSearch/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace ArtistSearch
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Artist> artistsList = new List<Artist>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSearchArtist_Click(object sender, RoutedEventArgs e)
+        {
+            //If theres an arist entered
+            if (TbxEnterArtist.Text.Length > 0)
+            {
+                //Run Query to search DynamoDB artist table for matching artists
+                artistsList = GetMatchingArtists(TbxEnterArtist.Text);
+
+                //Output number of matching artists found to TbkErrorOutput
+                TbkErrorOutput.Foreground = new SolidColorBrush(Colors.Green);
+                TbkErrorOutput.Text = (artistsList.Count + " matching artists found");
+
+                //Output all matching artists to LbxMatchingArtists
+            }
+            else
+            {
+                //Output error because no search term
+                TbkErrorOutput.Foreground = new SolidColorBrush(Colors.Red);
+                TbkErrorOutput.Text = "Please enter an aritst name";
+            }
+        }
+        private List<Artist> GetMatchingArtists(string searchTerm)
+        {
+            List<Artist> matchingArtists = new List<Artist>();
+
+            return matchingArtists;
+        }
+
+        class Artist
+        {
+            public string Name { get; set; }
+            public List<Album> Albums { get; set; }
+        }
+        class Album
+        {
+            public string AlbumTitle { get; set; }
+            public List<string> Songs { get; set; }
         }
     }
 }
